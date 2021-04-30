7ce93b0135a17d050cdaf8dce79fb1eeeced530b
YourtionGuo <yourtion@gmail.com>
2021-04-30 21:41:06 +0800

    ToyReact: 更新 build 产物

--- toy-react/vite.config.js
+++ toy-react/vite.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 export default {
   esbuild: {
     jsxFactory: 'createElement',
-    jsxInject: `import { createElement } from './toy-react';`
-  }
-}
+    jsxInject: `import { createElement } from './toy-react';`,
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/toy-react.ts'),
+      name: 'toy-react',
+    },
+  },
+};
