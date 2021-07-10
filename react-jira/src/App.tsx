43266d2800d29f7283ef5ea1ef0610a5b3ff5015
YourtionGuo <yourtion@gmail.com>
2021-07-10 15:16:45 +0800

    feat: 添加 ErrorBoundary

--- react-jira/src/App.tsx
+++ react-jira/src/App.tsx
@@ -2,9 +2,15 @@ import { useAuth } from 'context/auth-context';
 import { UnauthApp } from 'unauth-app';
 import { AuthedApp } from './authed-app';
 import './App.css';
+import { ErrorBoundary } from 'components/error-boundary';
+import { FullPageErrorFallback } from 'components/lib';
 function App() {
   const { user } = useAuth();
-  return <div className="App">{user ? <AuthedApp /> : <UnauthApp />}</div>;
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>{user ? <AuthedApp /> : <UnauthApp />}</ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
